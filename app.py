0e4d223bf9e5d6384b62dd7e67c1dd17674c48b9
Machew <92878666+lilmatthew2828@users.noreply.github.com>
2024-04-24 02:01:34 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ app.register_blueprint(views, url_prefix="/")
 # app_website.register_blueprint(views, url_prefix="/views/home")
 if __name__ == '__main__':
     app.run(debug=True,port=9000) # we set debug = True because whenevever we change a file inside the flask app it will automatically refresh the app
-    
+     
--- views.py
+++ views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request
-
+from flask import Blueprint, render_template, request, redirect, url_for
+import csv
 views = Blueprint(__name__, "views")
 
 @views.route("/")
@@ -8,30 +8,18 @@ def home():
 
 @views.route("/login", methods=["GET", "POST"])
 def login():
-    correct_length = False
-    has_upper = False
-    has_lower = False
-    has_digit = False
     if request.method == "POST":
         # Handle form submission
         email = request.form.get("email")
         password = request.form.get("password")
         # Perform login authentication here
-        while(correct_length == False or has_upper == False or has_lower == False or has_digit == False):
-            if len(password() > 7):
-                correct_length = True
-            elif email == "mattkilp5@gmail.com" and password == "mattkilp5@gmail.com":
-                return "Logged in successfully"  # You can customize this response
-            else:
-                return "Invalid email or password"  # You can customize this response
-            for each_character in password:
-                if each_character.isdigit():
-                    has_digit = True
-                elif each_character.islower():
-                    has_lower = True
-                elif each_character.isupper():
-                    has_upper = True
-        return "Logged in successfully"  # You can customize this response
+        # Password validation
+        if email == "mattkilp5@gmail.com" and password == "matt":
+            return "Login successful"  # Indicate successful login
+        else:
+            return "Invalid email or password"  # You can customize this response
+
     else:
         # Render login page for GET request
         return render_template("login.html")
+    
